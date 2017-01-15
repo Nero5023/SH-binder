c1f8a1541de458af83b54f9e395478537752df41
Nero Zuo <502350123@qq.com>
2017-01-15 21:23:29 +0800

    fix python3 problem

--- simpleserver.py
+++ simpleserver.py
@@ -1,16 +1,30 @@
-from twisted.internet import reactor, protocol
+from twisted.internet import reactor
+from twisted.internet.protocol import Protocol, Factory
 
-class Echo(protocol.Protocol):
+PORT = 8000
+
+class Echo(Protocol):
     """docstring for Echo"""
+
+
+    def connectionMade(self):
+        print("Got connection from", self.transport.client)
+
+    def connectionLost(self, reason):
+        print(self.transport.client, 'disconnected')
 
     def dataReceived(self, data):
         print(data)
-        self.transport.write(data)
+        # self.factory.send("%s" % (data))
+        replay = "%s" % (data.decode(encoding='UTF-8'))
+        replay = bytes(replay, 'utf-8')
+        self.transport.write(replay)
 
 def main():
-    factory = protocol.ServerFactory()
+    print("ServerStarting")
+    factory = Factory()
     factory.protocol = Echo
-    reactor.listenTCP(8000, factory)
+    reactor.listenTCP(PORT, factory)
     reactor.run()
 
 if __name__ == '__main__':
