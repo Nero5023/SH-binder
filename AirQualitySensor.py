1396a28a2d44d2984c3d281e4a1491ffdef253bd
Nero Zuo <502350123@qq.com>
2017-01-17 15:05:41 +0800

    fix bug

--- AirQualitySensor.py
+++ AirQualitySensor.py
@@ -91,4 +91,4 @@ if __name__ == '__main__':
         sensor.getAirQuality()
         print("\n %s"%(sensor.ser.inWaiting()))
         print("------------------------")
-        sleep(1)
+        time.sleep(1)
