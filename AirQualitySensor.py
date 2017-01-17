f976304fa0fc5d57a67607d82305b6e15755bf92
Nero Zuo <502350123@qq.com>
2017-01-17 15:07:01 +0800

    fix bug

--- AirQualitySensor.py
+++ AirQualitySensor.py
@@ -72,6 +72,7 @@ class AirQualitySensor(metaclass=Singleton):
         if bufferLength >= 32:
             effectiveFrame = bufferLength/32
             readLength = effectiveFrame * 32
+            print readLength
             data = self.ser.read(readLength)
             airQuality = decode(data[readLength-32:])
             return airQuality
